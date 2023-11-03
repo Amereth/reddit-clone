cc9607b6f130cccc19fb7d771cd7d1f8007956ba
Oleksandr-Melnyk-Artelogic <omelnyk@artelogic.net>
2023-11-03 23:08:17 +0200

    setup

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,10 +1,12 @@
-/** @type {import("prettier").Config} */
-const config = {
+module.exports = {
   tabWidth: 2,
   semi: false,
   singleQuote: true,
   jsxSingleQuote: true,
-  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  plugins: [
+    require.resolve('prettier-plugin-tailwindcss'),
+    '@trivago/prettier-plugin-sort-imports',
+  ],
+  importOrder: ['^@core/(.*)$', '^@server/(.*)$', '^@ui/(.*)$', '^[./]'],
+  importOrderSeparation: false,
 }
-
-module.exports = config
