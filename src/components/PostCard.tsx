2f1e5c2c9d5cfcf432117bdd6ccdf6a5576e59ad
Oleksandr-Melnyk-Artelogic <omelnyk@artelogic.net>
2023-11-11 18:48:24 +0200

    Refactor PostCard component to conditionally render author avatar

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -24,10 +24,12 @@ export const PostCard = ({ post }: PostCardProps) => {
       <div className='relative flex h-full flex-col gap-4 rounded-xl p-4'>
         <div className='flex flex-col gap-4'>
           <header className='flex items-center gap-4'>
-            <Avatar
-              src={post.author.imageUrl ?? ''}
-              className='min-w-min shrink-0 self-start'
-            />
+            {post.author.imageUrl && (
+              <Avatar
+                src={post.author.imageUrl}
+                className='min-w-min shrink-0 self-start'
+              />
+            )}
             <h2 className='line-clamp-2'>{post.title}</h2>
           </header>
         </div>
