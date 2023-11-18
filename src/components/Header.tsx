cb57dd2514a8cc7684b451a676d93a406cd4ba27
Oleksandr-Melnyk-Artelogic <omelnyk@artelogic.net>
2023-11-18 22:45:59 +0200

    added back button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,32 @@
 import { SignIn, SignInButton, useUser, UserButton } from '@clerk/nextjs'
 import { Button } from '@nextui-org/react'
+import { ChevronLeft } from 'lucide-react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export const Header = () => {
   const { isSignedIn } = useUser()
+  const router = useRouter()
+  const onGoBack = () => router.back()
 
   return (
     <>
       <header className='flex h-16 items-center border-b-1 px-4'>
         {isSignedIn && (
           <>
-            <Link className='mr-auto' href='/create-post'>
+            {router.route !== '/' && (
+              <Button isIconOnly onClick={onGoBack} className='mr-4'>
+                <ChevronLeft />
+              </Button>
+            )}
+
+            <Link href='/create-post'>
               <Button>create post</Button>
             </Link>
 
-            <UserButton />
+            <div className='ml-auto'>
+              <UserButton />
+            </div>
           </>
         )}
 
